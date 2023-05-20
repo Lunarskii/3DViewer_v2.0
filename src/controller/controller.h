dd3c67159d7b8e72597fa93b1aaef3a2fe67dbe8
Lunarskii <lunarskii0@gmail.com>
2023-05-20 20:44:41 +0300

    the Singleton pattern has been added, and define has also been added

--- src/controller/controller.h
+++ src/controller/controller.h
@@ -1,9 +1,12 @@
+#ifndef CPP4_3DVIEWER_V2_0_CONTROLLER_CONTROLLER_H_
+#define CPP4_3DVIEWER_V2_0_CONTROLLER_CONTROLLER_H_
+
 #include "../model/model.h"
 
 class Controller 
 {
 public:
-    Controller(Model* m) : model(m) {}
+    Controller() : model(&Model::getInstance()) {}
 
     void setFileName(std::string fileName);
     int getError();
@@ -13,3 +16,5 @@ public:
 private:
     Model* model;
 };
+
+#endif  // CPP4_3DVIEWER_V2_0_CONTROLLER_CONTROLLER_H_
